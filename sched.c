0172301b2c8b2ccea0d078f4c1f72487726a7853
cnorick <cnathanorick@gmail.com>
2017-02-06 22:21:52 -0500

    Implement first try at new_scheduler

--- sched.c
+++ sched.c
@@ -1,11 +1,13 @@
 #include "sched.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 //Simulate a timer interrupt from hardware. This should initiate
 ////the context switch procedure
 //// - Context switch must save active process' state into the PROCESS structure
 //// - Context switch must load the next process' state into the scheduler
 void timer_interrupt(SCHEDULER *s) {
-
+	
 }
 
 ////Create a new scheduler
@@ -14,8 +16,34 @@ void timer_interrupt(SCHEDULER *s) {
 //// - Schedule process 1 (init)
 //// - Set process 1 to current process
 //// - Return the created scheduler
-SCHEDULER *new_scheduler(PROCESS_CODE_PTR(code)) {
+SCHEDULER *new_scheduler(PROCESS_CODE_PTR(initCode)) {
+	// Create new scheduler.
+	SCHEDULER *s = (SCHEDULER*) malloc(sizeof(SCHEDULER));
 
+	// Create process 1.
+	PROCESS* init = (PROCESS*) malloc(sizeof(PROCESS));
+	init->name = "init";
+	init->pid = 1;
+	init->switched = 0;
+	init->total_cpu_time = 0;
+	init->switched_cpu_time = 0;
+	init->job_time = -1;
+	init->saved_registers = *((REGISTER_FILE*) malloc(sizeof(REGISTER_FILE)));
+	init->state = PS_NONE;
+	init->init = initCode;
+	//init->step = 
+
+
+	// Load init into the scheculer and set it as running.
+	s->process_list[0] = *init;
+	s->current = 0;
+	//s->scheduler_algorithm = 
+	s->active_registers = init->saved_registers;
+
+	s->process_list[s->current].state = PS_RUNNING;
+	s->process_list[s->current].init(&s->active_registers, (RETURN*) malloc(sizeof(RETURN)));
+
+	return s;
 }
 
 ////Duplicate a process (copied, NOT SHARED)
@@ -31,3 +59,6 @@ int fork(SCHEDULER *s, PID src_p) {
 int exec(SCHEDULER *s, PID pid, const char *new_name, PROCESS_CODE_PTR(init), PROCESS_CODE_PTR(step), int job_time) {
 
 }
+
+
+
