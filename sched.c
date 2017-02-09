1240e3a0b467d10d579e1a4a9cf6c85305908d52
Jacob Scott Allen <jallen98@hydra12.eecs.utk.edu>
2017-02-09 13:32:47 -0500

    Copied over changes made when making pseudocode functions

--- sched.c
+++ sched.c
@@ -1,53 +1,53 @@
-// made a change so it would commit, ignore
 #include "sched.h"
 #include <stdlib.h>
 #include <stdio.h>
 
-void saveActiveProcessState(SCHEDULER *s);
-void goToNextProcess(SCHEDULER *s);
+void saveActiveProcessRegisters(SCHEDULER *s);
+void updateCurrentProcessTotalCPUTime(SCHEDULER *s);
+void updateAllProcessSleepTimeRemaining(SCHEDULER *s);
+void setCurrentProcessToSleeping(SCHEDULER *s);
+void setNewCurrentProcess(SCHEDULER *s);
+void setNextProcessToCurrent(SCHEDULER *s, unsigned int nextProcess);
+void setCurrentProcessToRunning(SCHEDULER *s);
+void loadActiveProcessRegisters(SCHEDULER *s);
+void updateSwitchedCPUTime(SCHEDULER *s);
+void initializeProcessSwitchedCPUTime(SCHEDULER *s);
+void incrementCurrentProcessSwitched(SCHEDULER *s);
+void callProcessCodePtrStep(SCHEDULER *s);
+PID add_process(SCHEDULER *s, PROCESS *p);
+PROCESS *get_process(SCHEDULER *s, PID pid);
+
 
 //Simulate a timer interrupt from hardware. This should initiate
 ////the context switch procedure
 //// - Context switch must save active process' state into the PROCESS structure
 //// - Context switch must load the next process' state into the scheduler
 void timer_interrupt(SCHEDULER *s) {
-	// call a function to save the active process's state 
-	// into process structure
-		// call process code ptr
-			// how? What does this do?
-		// update total_cpu_time to prev + clock cycle
-		// update job_time
-			// what is this?
-		// save the current registers to saved_registers
-		// set the process state as sleeping
+	saveActiveProcessRegisters(s);
+	updateCurrentProcessTotalCPUTime(s);
+	updateAllProcessSleepTimeRemaining(s);
+	// setCurrentProcessToSleeping(s); maybe not?
+
+	unsigned int oldCurrentProcess = s -> current; 
 	
-	// function should also increment  current -> switched
-		// is switching to a process and from an object both
-		// things that increment current -> switched?
+	setNewCurrentProcess(s);
+
+	unsigned int newCurrentProcess = s -> current;
 	
-	// switch statement on s -> scheduler_algorithm
-	// call the other functions that actually implement the algorithm
-		// be sure to load the next 
-}
+	// setCurrentProcessToRunning(s); maybe not?
+	loadActiveProcessRegisters(s);
 
-void saveActiveProcessState(SCHEDULER *s){
-
-}
-
-void goToNextProcess(SCHEDULER *s){
-	switch(s -> scheduler_algorithm){
-		case SA_ROUND_ROBIN:
-			break;
-		case SA_FAIR:
-			break;
-		case SA_FCFS:
-			break;
-		case SA_SJF:
-			break;
-		default:
-			break;
+	if(newCurrentProcess == oldCurrentProcess){
+		updateSwitchedCPUTime(s);
+	}
+	else{
+		initializeProcessSwitchedCPUTime(s);
+		incrementCurrentProcessSwitched(s);
+		callProcessCodePtrStep(s);
 	}
 }
+
+
 
 ////Create a new scheduler
 ////This function needs to
@@ -56,40 +56,52 @@ void goToNextProcess(SCHEDULER *s){
 //// - Set process 1 to current process
 //// - Return the created scheduler
 SCHEDULER *new_scheduler(PROCESS_CODE_PTR(initCode)) {
-	// Create new scheduler.
-	SCHEDULER *s = (SCHEDULER*) malloc(sizeof(SCHEDULER));
+    // Create new scheduler.
+    SCHEDULER *s = (SCHEDULER*) malloc(sizeof(SCHEDULER));
 
-	// Create process 1.
-	PROCESS* init = (PROCESS*) malloc(sizeof(PROCESS));
-	init->name = "init";
-	init->pid = 1;
-	init->switched = 0;
-	init->total_cpu_time = 0;
-	init->switched_cpu_time = 0;
-	init->job_time = -1;
-	init->saved_registers = *((REGISTER_FILE*) malloc(sizeof(REGISTER_FILE)));
-	init->state = PS_NONE;
-	init->init = initCode;
-	//init->step = 
+    // Create process 1.
+    PROCESS* init = (PROCESS*) malloc(sizeof(PROCESS));
+    init->name = "init";
+    init->pid = 1;
+    init->switched = 0;
+    init->total_cpu_time = 0;
+    init->switched_cpu_time = 0;
+    init->job_time = -1;
+    init->saved_registers = *((REGISTER_FILE*) malloc(sizeof(REGISTER_FILE)));
+    init->state = PS_NONE;
+    init->init = initCode;
 
+    // Initialize process list with PS_NONE.
+    PID i;
+    for(i = 0; i < MAX_PROCESSES; i++)
+        s->process_list[i].state = PS_NONE;
 
-	// Load init into the scheculer and set it as running.
-	s->process_list[0] = *init;
-	s->current = 0;
-	//s->scheduler_algorithm = 
-	s->active_registers = init->saved_registers;
+    // Load init into the scheculer and set it as running.
+    s->current = add_process(s, init); // Should add it at 0, returning pid 1.
+    s->active_registers = init->saved_registers;
 
-	s->process_list[s->current].state = PS_RUNNING;
-	s->process_list[s->current].init(&s->active_registers, (RETURN*) malloc(sizeof(RETURN)));
+    PROCESS *currentProcess = get_process(s, s->current);
+    currentProcess->state = PS_RUNNING;
+    currentProcess->init(&s->active_registers, (RETURN*) malloc(sizeof(RETURN)));
 
-	return s;
+    return s;
 }
 
 ////Duplicate a process (copied, NOT SHARED)
 ////The passed scheduler forks a process that is a duplicate
 ////of src_p.
 int fork(SCHEDULER *s, PID src_p) {
+    PROCESS *parent = get_process(s, src_p);
 
+    PID newPid = add_process(s, parent);
+    PROCESS *child = get_process(s, newPid);
+
+    // Do we copy over time as well or make it 0 again?
+
+    // Are we supposed to change this too?
+    //child->state = PS_SLEEPING;
+
+    return child->pid;
 }
 
 ////Overwrite the current process with the new information
@@ -101,3 +113,93 @@ int exec(SCHEDULER *s, PID pid, const char *new_name, PROCESS_CODE_PTR(init), PR
 
 
 
+
+
+
+/********************
+ * Helper Functions *
+ *******************/
+
+// Adds a process to the scheduler in the first open process slot. Returns the pid of the new process (index + 1) or -1 if no slot was found.
+PID add_process(SCHEDULER *s, PROCESS *p) {
+    PID i;
+    for(i = 0; i < MAX_PROCESSES; i++) {
+        if(s->process_list[i].state == PS_NONE) {
+            s->process_list[i] = *p;
+            return i + 1;
+        }
+    }
+
+    return -1;
+}
+
+// Returns the PROCESS with processId pid.
+PROCESS *get_process(SCHEDULER *s, PID pid) {
+    // process id is 1-based. array is 0-based.
+    return &s->process_list[pid - 1]; 
+}
+
+void saveActiveProcessRegisters(SCHEDULER *s){
+
+}
+
+void updateCurrentProcessTotalCPUTime(SCHEDULER *s){
+
+}
+
+void updateAllProcessSleepTimeRemaining(SCHEDULER *s){
+
+}
+
+void setCurrentProcessToSleeping(SCHEDULER *s){
+
+}
+
+// all this does is choose set s -> current
+// to what the next process should be
+void setNewCurrentProcess(SCHEDULER *s){
+	switch(s -> scheduler_algorithm){
+		case SA_ROUND_ROBIN:
+			break;
+		case SA_FAIR:
+			break;
+		case SA_FCFS:
+			break;
+		case SA_SJF:
+			break;
+		default:
+			// maybe output an error?
+			break;
+	}
+
+	// setNextProcessToCurrent(s, nextProcess);
+}
+
+
+void setNextProcessToCurrent(SCHEDULER *s, unsigned int nextProcess){
+
+}
+
+void setCurrentProcessToRunning(SCHEDULER *s){
+
+}
+
+void loadActiveProcessRegisters(SCHEDULER *s){
+
+}
+
+void updateSwitchedCPUTime(SCHEDULER *s){
+
+}
+
+void initializeProcessSwitchedCPUTime(SCHEDULER *s){
+
+}
+
+void incrementCurrentProcessSwitched(SCHEDULER *s){
+
+}
+
+void callProcessCodePtrStep(SCHEDULER *s){
+
+}
