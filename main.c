5c6dab7f49298f4846639f0c218128f81e2ab16f
cnorick <cnathanorick@gmail.com>
2017-02-20 14:13:41 -0500

    Add mutex support
    Note that none of this code should have any effect on the rest of
    project. So it should be safe to merge.

--- main.c
+++ main.c
@@ -6,22 +6,24 @@
 
 
 int main() {
-    printf("\n\n===================Testing Round Robin=====================\n\n");
-    roundRobinTest();
-    printf("\n\n===================Testing FCFS============================\n\n");
-    fcfsTest();
-    printf("\n\n===================Testing Fair============================\n\n");
-    fairTest();
-    printf("\n\n===================Testing SJF============================\n\n");
-    sjfTest();
-    printf("\n\n===================Testing Registers============================\n\n");
-    registerTest();
-    printf("\n\n===================Testing Sleeping============================\n\n");
-    sleepTest();
-    printf("\n\n===================Testing Exiting and Adding More============================\n\n");
-    exitTest();
-    printf("\n\n===================Testing Marzzzz============================\n\n");
-    marzTest();
+//    printf("\n\n===================Testing Round Robin=====================\n\n");
+//    roundRobinTest();
+//    printf("\n\n===================Testing FCFS============================\n\n");
+//    fcfsTest();
+//    printf("\n\n===================Testing Fair============================\n\n");
+//    fairTest();
+//    printf("\n\n===================Testing SJF============================\n\n");
+//    sjfTest();
+//    printf("\n\n===================Testing Registers============================\n\n");
+//    registerTest();
+//    printf("\n\n===================Testing Sleeping============================\n\n");
+//    sleepTest();
+//    printf("\n\n===================Testing Exiting and Adding More============================\n\n");
+//    exitTest();
+//    printf("\n\n===================Testing Marzzzz============================\n\n");
+//    marzTest();
+
+    mutexTest();
  
 
 	return 0;
